093cc3ab6ed37afd1f8931277fa5f254900d14a6
Kali Soutar <kalisoutar@gmail.com>
2021-10-08 18:27:09 -0500

    Everything but Exercises 4 and 5

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,8 @@
- 
- window.onload = function(){
+
+ window.onload = function(){startGame()}
+ function startGame(){
     let gamelayout = document.querySelectorAll("#board div");
+    const restartGame = document.getElementsByClassName
     for(let i=0; i<gamelayout.length; i++){
         gamelayout[i].classList.add("square");
         const xClass= "X"
@@ -13,17 +15,35 @@
         const space = ele.target
         const currentClass = circleTurn ? oClass :  xClass
         placePlay(space,currentClass)
-        console.log(currentClass)
+        console.log(circleTurn)
         gameplay.push(currentClass)
         console.log(gameplay)
         circleTurn =! circleTurn
-
-         
-    }}
-    function placePlay(space,currentClass){
-        space.textContent = currentClass
-        space.classList.add(currentClass)
-    }
     
+    }
+    gamelayout.forEach(element => { element.addEventListener("mouseover",hoverchange)     
+    })
+    function hoverchange(ele){
+        ele.target.classList.add("hover")
+    }
+    gamelayout.forEach(element => { element.addEventListener("mouseout",hoverout)     
+    })
+    function hoverout(ele){
+        ele.target.classList.remove("hover")
+    }
+    }
+    gamelayout.forEach(element)
+    { element.addEventListener("moseover",function(element){
+        element.target.add(hover)
+    })
+    
+    restartGame.addEventListener("click",startGame)
+    console.log(restartGame)
  }
+}
+function placePlay(space,currentClass){
+    space.textContent = currentClass
+    space.classList.add(currentClass)
+    space.style.pointerEvents = "none"
+}
 
