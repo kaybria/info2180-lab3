c092244677f392fa5cbe0b5145b65bd786107181
Kali Soutar <kalisoutar@gmail.com>
2021-10-08 17:05:21 -0500

    Exercise 2 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,29 @@
+ 
  window.onload = function(){
     let gamelayout = document.querySelectorAll("#board div");
     for(let i=0; i<gamelayout.length; i++){
-        gamelayout[i].classList.add('square');
-        
+        gamelayout[i].classList.add("square");
+        const xClass= "X"
+        const  oClass= "O"
+        let circleTurn 
+    gamelayout.forEach(element => { element.addEventListener("click",isClicked)     
+    })
+    const gameplay = []
+    function isClicked(ele){
+        const space = ele.target
+        const currentClass = circleTurn ? oClass :  xClass
+        placePlay(space,currentClass)
+        console.log(currentClass)
+        gameplay.push(currentClass)
+        console.log(gameplay)
+        circleTurn =! circleTurn
+
+         
     }}
+    function placePlay(space,currentClass){
+        space.textContent = currentClass
+        space.classList.add(currentClass)
+    }
+    
+ }
+
